f8576a2434058dde49d589580d3cc83c9d923957
Divansh Singh <119401802+divanshsinghsaini123@users.noreply.github.com>
2024-01-12 20:30:37 +0530

    Add files via upload

---
+++ balanceParentheses.cpp
@@ -0,0 +1,65 @@
+// If N=2 , means their must be 2 opening and 2 closing brackets 
+   // you can not place a closing at 1st place 
+   // you have two cases 
+        // place a opening if no. of opening less then N
+        // place a closing if no. of closing less then opening ;
+// we have to print the all possible combinations
+#include<iostream>
+using namespace std;
+void parentheses(char*a, int i, int o , int c, int n ){
+    // base case 
+    if(i==2*n){  
+        a[i]='\0';
+         cout<<a<<endl;
+       return;
+    }
+
+    //recursive case
+    if (o<n)
+    {
+        a[i]='(';
+        parentheses(a,i+1,o+1,c,n);
+    }
+    if (o>c)
+    {
+        a[i]=')';
+        parentheses(a,i+1,o,c+1,n);
+    }
+    
+}
+int noOfparenthese(int i,int o,int c , int n){
+    // base case 
+    int t;
+    if (i==2*n)
+    {
+        // t=t+1;
+        return 1;
+    }
+        
+        // cout<<"hello"<<endl;
+    // recursive case
+    if (o<n)
+    {
+        t= t+noOfparenthese(i+1,o+1,c,n);
+    }
+    if (o>c)
+    {
+         t=t+ noOfparenthese(i+1,o,c+1,n);
+    }
+    return t;
+   
+}
+
+int main(){
+    int n;
+    cin>>n;
+    char a[]={};
+    parentheses(a,0,0,0,n);
+    
+    // but if I want to calculate the total number of possible balanced parentheses 
+    cout<<noOfparenthese(0,0,0,n);
+
+
+
+    return 0;
+}
